c42a9aaa7eee60441a252ae56fff8502082b0b0f
mepstein3639 <EpsteinM9814@sduhsd.lan>
2016-09-06 10:58:31 -0700

    Added Files

---
+++ APLesson_01/FirstProgram.java
@@ -0,0 +1,64 @@
+public class FirstProgram
+{
+	public static void main(String[]args)
+	{
+		System.out.println("                                               _______");
+		System.out.println("                                         .,add88YYYYY88ba,");
+		System.out.println("                                    .,adPP\"\"\'         `\"Yba___,aaadYPPba,");
+		System.out.println("                                .,adP\"\"                .adP\"\"\"\"\'     .,Y8b");
+		System.out.println("                             ,adP\"\'                __  d\"'     .,ad8P\"\"Y8I");
+		System.out.println("                          ,adP\"\'                  d88b I  .,adP\"\"\'   ,d8I\'");
+		System.out.println("                        ,adP\"                     Y8P\" ,adP\"\'    .,adP\"\'");
+		System.out.println("                       adP\"                        \"\' dP\"     ,adP\"\"\'");
+		System.out.println("                    ,adP\"                             P    ,adP\"\'");
+		System.out.println("            .,,aaaad8P\"                                 ,adP\"");
+		System.out.println("       ,add88PP\"\"\"\"'                                  ,dP\"");
+		System.out.println("    ,adP\"\"'                                         ,dP\"");
+		System.out.println("  ,8P\"'                                            d8\"");
+		System.out.println(",dP'                                              dP'");
+		System.out.println("`\"Yba                                             Y8");
+		System.out.println("  `\"Yba                                           `8,");
+		System.out.println("    `\"Yba,                                         8I");
+		System.out.println("       `\"8b                                        8I");
+		System.out.println("         dP                              __       ,8I");
+		System.out.println("        ,8\'                            ,d88b,    ,d8\'");
+		System.out.println("        dP                           ,dP\'  `Yb, ,d8\'");
+		System.out.println("       ,8\'                         ,dP\"      `\"Y8P\'");
+		System.out.println("       dP                        ,8P\"");
+		System.out.println("      ,8\'                      ,dP\"    ");
+		System.out.println("      dP                     ,dP\"      ");
+		System.out.println("     ,8\'                    ,8P\'");
+		System.out.println("     I8                    dP\"");
+		System.out.println("     IP                   dP\'");
+		System.out.println("     dI                  dP\'");
+		System.out.println("    ,8\'                 dP\'");
+		System.out.println("    dI                 dP\'");
+		System.out.println("    8\'                ,8\'");
+		System.out.println("    8                ,8I");
+		System.out.println("    8                dP\'");
+		System.out.println("    8               ,8\'");
+		System.out.println("    8,              IP\'");
+		System.out.println("    Ib             ,dI");
+		System.out.println("    `8             I8\'");
+		System.out.println("     8,            8I");
+		System.out.println("     Yb            I8");
+		System.out.println("     `8,           I8");
+		System.out.println("      Yb           I8");
+		System.out.println("      `Y,          I8");
+		System.out.println("       Ib          I8,");
+		System.out.println("       `Ib         `8I");
+		System.out.println("        `8,         Yb");
+		System.out.println("         I8,        `8,");
+		System.out.println("         `Yb,        `8a");
+		System.out.println("          `Yb         `Yb,");
+		System.out.println("           I8          `Yb,");
+		System.out.println("           dP            `Yb,");
+		System.out.println("          ,8\'              `Yb,");
+		System.out.println("          dP                 `Yb,");
+		System.out.println("         d88baaaad88ba,        `8,");
+		System.out.println("            `\"\"\'   `Y8ba,     ,dI");
+		System.out.println("                       `\"\"Y8baadP\'");
+		System.out.println("                            `\"\"\'");
+
+	}
+}
---
+++ APLesson_01/SecondProgram.java
@@ -0,0 +1,13 @@
+public class SecondProgram
+{
+	public static void main(String[]args)
+	{
+		System.out.println("        |\\ |\\");
+		System.out.println("        \\ \\| |");
+		System.out.println("         \\ | |");
+		System.out.println("       .--''/");
+		System.out.println("      /o     \\");
+		System.out.println("      \\      /");
+		System.out.println("   {>o<}=\'");	
+	}
+}
