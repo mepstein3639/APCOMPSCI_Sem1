b8f19d17674e50f2323d0a617dece58c5178b831
mepstein3639 <EpsteinM9814@sduhsd.lan>
2016-09-12 10:39:32 -0700

    Fixed lesson

---
+++ APLesson_03/Lesson_03.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+
+public class Lesson_03
+{
+	public static void main(String[]args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("What is your name? ");
+		String name = keyboard.next();
+		System.out.println("Nice! " + name + " is the coolest name ever!");
+	
+	}
+}
