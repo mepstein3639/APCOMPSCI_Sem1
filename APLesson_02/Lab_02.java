7b920e4d1b0cd81fff0269d96246974def6b4a41
mepstein3639 <EpsteinM9814@sduhsd.lan>
2016-09-30 11:04:48 -0700

    Fixed lab_02

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,28 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int l = 16;
+		int w = 10;
+		int recArea = l * w;
+		
+		System.out.println("The area of your rectangle is" +" "+ recArea);
+		
+		String name = "Michael Epstein";
+		String address = "1234 Main Street";
+		String city = "San Diego, Ca";
+		String zipcode = "92130";
+		
+		System.out.println(name);
+		System.out.println(address);
+		System.out.println(city);
+		System.out.println(zipcode);
+		
+		int len = 4;
+		int wid = 3;
+		int hei = 2;
+		
+		System.out.println(" The surface are is " + 2*(wid * hei + len * hei + len * wid));
+		
+	}
+}
